42d850d56b3c775847adc72affb2d8e4aed8b6a6
pbornay <pbornay@bravurasolutions.com>
2023-10-20 21:43:46 +1100

    add post request

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
+const port = 3000;
 
 app.listen(3000, () => console.log("Server up and running"));
 
-var todos = ['buy the milk', 'rent a car', 'feed the cat'];
+var todos = [{id:1, title:'buy the milk'}, {id:2, title:'rent a car'}, {id:3, title:'feed the cat'}];
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   return res.status(200).json(todos);
 })
 
-// tutorial https://www.codingame.com/playgrounds/1064/building-a-basic-todo-list-rest-api-in-node-js-with-express/key-concepts
+app.post('/', (request, response) => {
+  // var newTodo = JSON.parse(request.body); don't even need to use json.parse 
+  var newTodo = request.body;
+  console.log(newTodo);
+  newTodo.id = todos.length +1;
+  todos.push(newTodo);
+  response.status(201).json(newTodo);
+});
+
+// tutorial https://www.codingame.com/playgrounds/1064/building-a-basic-todo-list-rest-api-in-node-js-with-express/discover-express
