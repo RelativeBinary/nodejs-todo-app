e35d608ca5410dc202f8c5f7db27b7b175c048c3
RelativeBinary <patrick.bornay@gmail.com>
2023-10-21 14:24:23 +1100

    add in mem db and refactor implementation

--- index.js
+++ index.js
@@ -1,51 +1,54 @@
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
+const Todo = require("./models/todo");
 
-app.listen(3000, () => console.log("Server up and running"));
-
-var todos = [{id:1, title:'buy the milk'}, {id:2, title:'rent a car'}, {id:3, title:'feed the cat'}];
+app.listen(port, () => console.log("Server up and running"));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  return res.status(200).json(todos);
-})
+app.get("/", (req, res) => {
+  Todo.all((err, todos) => res.status(200).json(todos));
+});
 
-app.post('/', (request, response) => {
-  // var newTodo = JSON.parse(request.body); don't even need to use json.parse 
+app.post("/", (request, response) => {
+  // var newTodo = JSON.parse(request.body); don't even need to use json.parse
   var newTodo = request.body;
   console.log(newTodo);
-  newTodo.id = todos.length +1;
-  todos.push(newTodo);
+  Todo.add(newTodo);
   response.status(201).json(newTodo);
 });
 
-app.put('/:id', (req, res) => {
+app.put("/:id", (req, res) => {
   var id = req.params.id;
-  console.log(id);
-  if (todos[id - 1]) {
-    todos[id - 1] = req.body;
-    res.status(204).send();
-  } else {
-    res.status(404, 'The todo is not found').send();
-  }
+  var updatedTodo = req.body;
+  updatedTodo.id = parseInt(id);
+  Todo.update(updatedTodo, (err, data) => {
+    if (err) {
+      res.status(404, "The task is not found").send();
+    } else {
+      res.status(204).send(data);
+    }
+  });
 });
 
-app.delete('/:id', (req, res) => {
+app.delete("/:id", (req, res) => {
   var id = parseInt(req.params.id);
-  if (todos.some(todo => todo.id === id)) {
-    todos = todos.filter(todo => todo.id !== id);
-    res.status(200).send();
-  } else {
-    res.status(404, 'The todo is not found').send();
-  }
-})
+  Todo.delete(id, (err) => {
+    if(err){
+      res.status(404).send();
+    }else{
+          res.status(200).send();
+    }
+  });
+});
 
 // todo ids are not truly unique
+// todo add todo service
+// todo add tests
 // tutorial https://www.codingame.com/playgrounds/1064/building-a-basic-todo-list-rest-api-in-node-js-with-express/discover-express
